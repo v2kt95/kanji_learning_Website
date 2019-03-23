3f8cc5386bac8201bca240c152c5452174fab1be
vuongdv3 <v2kt95@gmail.com>
2019-03-23 11:38:33 +0700

    update cronjob

--- update_script.py
+++ update_script.py
@@ -5,10 +5,8 @@ import django
 django.setup()
 from japan.models import Kanji,Word
 
-kanjis = Kanji.objects.get(id=153)
-kanjis.remember_point -= kanjis.strokes
-kanjis.save()
-# for kanji in kanjis:
-# 	kanji.remember_point -= kanji.strokes
-# 	kanji.save()
+kanjis = Kanji.objects.all()
+for kanji in kanjis:
+	kanji.remember_point -= kanji.strokes
+	kanji.save()
 print("updated kanji...\n")
