d562b2b5e940432be23809c20bb84aaac68b776b
Do Viet Vuong <vuongdv@kaopiz.com>
2019-08-18 23:00:53 +0700

    update script

--- update_script.py
+++ update_script.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nihongo1.settings")
 import django
+
 django.setup()
-from japan.models import Kanji,Word
+from japan.models import Kanji
+from grammar.models import Grammar
 
 kanjis = Kanji.objects.all()
+grammars = Grammar.objects.all()
 for kanji in kanjis:
-	if kanji.day_count == 1:
-		kanji.day_count = kanji.day_down
-		if kanji.level > 1:
-			kanji.level -= 1
-		kanji.save()
-	else:
-		print("before :",kanji.day_count)
-		kanji.day_count -= 1		
-		kanji.save()
-		print("after :",kanji.day_count)
-print("updated kanji...\n")
+    if kanji.day_count == 1:
+        kanji.day_count = kanji.day_down
+        if kanji.level > 1:
+            kanji.level -= 1
+        kanji.save()
+    else:
+        print("before :", kanji.day_count)
+        kanji.day_count -= 1
+        kanji.save()
+        print("after :", kanji.day_count)
+
+for grammar in grammars:
+    if grammar.day_count == 1:
+        grammar.day_count = grammar.day_down
+        if grammar.level > 1:
+            grammar.level -= 1
+        grammar.save()
+    else:
+        print("before :", grammar.day_count)
+        grammar.day_count -= 1
+        grammar.save()
+        print("after :", grammar.day_count)
+print("updated kanji, grammar...\n")
