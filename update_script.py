5850d40f732c608d8ca6f5d5151d03d46cb45327
Do Viet Vuong <vuong@spinshell.com>
2020-03-30 23:14:43 +0900

    Update kanji script

--- update_script.py
+++ update_script.py
@@ -8,27 +8,27 @@ django.setup()
 from japan.models import Kanji
 from grammar.models import Grammar
 
-kanjis = Kanji.objects.all()
-grammars = Grammar.objects.all()
-for kanji in kanjis:
-    if kanji.day_count == 1:
-        kanji.day_count = kanji.day_down
-        if kanji.level > 1:
-            kanji.level -= 1
-        kanji.save()
-    else:
-        kanji.day_count -= 1
-        kanji.save()
+# kanjis = Kanji.objects.all()
+# grammars = Grammar.objects.all()
+# for kanji in kanjis:
+#     if kanji.day_count == 1:
+#         kanji.day_count = kanji.day_down
+#         if kanji.level > 1:
+#             kanji.level -= 1
+#         kanji.save()
+#     else:
+#         kanji.day_count -= 1
+#         kanji.save()
 
-for grammar in grammars:
-    if grammar.day_count == 1:
-        grammar.day_count = grammar.day_down
-        if grammar.level > 1:
-            grammar.level -= 1
-        grammar.save()
-    else:
-        print("before :", grammar.day_count)
-        grammar.day_count -= 1
-        grammar.save()
-        print("after :", grammar.day_count)
+# for grammar in grammars:
+#     if grammar.day_count == 1:
+#         grammar.day_count = grammar.day_down
+#         if grammar.level > 1:
+#             grammar.level -= 1
+#         grammar.save()
+#     else:
+#         print("before :", grammar.day_count)
+#         grammar.day_count -= 1
+#         grammar.save()
+#         print("after :", grammar.day_count)
 print("updated kanji, grammar...\n")
